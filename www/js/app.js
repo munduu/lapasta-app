77e07e48c69d23d82108dc27e179f6627cee9442
Boris <boris@4gotech.com.br>
2020-03-17 18:10:29 -0300

    start

---
+++ www/js/app.js
@@ -0,0 +1,13 @@
+//EXTERNO
+//var url_geral ="http://jackiebrown.dyndns.org:1020/app/";
+
+//INOVA
+//var url_geral ="http://192.168.1.100:8080/igestao/app/santoespeto_php/";
+
+//INTERNO
+//var url_geral ="http://192.168.0.2:1020/app/";
+
+var url_geral = "http://192.168.1.250/foodgo/app_novo/";
+
+//JOAO
+//var url_geral = "http://192.168.1.121:8081/igestao/app/santoespeto_php/";
